d1ea5b021b70d2f3e0d438420de7e0e57128974c
william-vega <willial1393@gmail.com>
2019-10-20 11:29:16 -0500

    fix image s3

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductsController extends Controller
 {
@@ -27,6 +29,20 @@ class ProductsController extends Controller
         $res = new CustomResponse();
         try {
             $item = Product::find($request->id);
+            if (strpos($request->image, 'http') === false) {
+                if ($item) {
+                    $item['image'] = str_replace('https://asorural.s3.us-east-2.amazonaws.com/', '', $item['image']);
+                    Storage::disk('s3')->delete($item['image']);
+                }
+                $base64_str = substr($request->image, strpos($request->image, ",") + 1);
+                $image = base64_decode($base64_str);
+                $timestamp = Carbon::now()->getTimestamp();
+                Storage::disk('s3')->put("images/productos/{$timestamp}.png", $image);
+                $url = Storage::disk('s3')->url("images/productos/{$timestamp}.png");
+                $request['image'] = $url;
+            } else {
+                unset($request['image']);
+            }
             if (!$item) {
                 $item = Product::create($request->all());
                 $res->status = 'Created';
